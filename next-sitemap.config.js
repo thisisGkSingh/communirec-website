d257dc7d7d650263236936d173c1c371990ed087
thisisGkSingh <gurkaran12@gmail.com>
2024-10-27 21:56:01 -0400

    Udpate

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: "https://www.imagicalabs.com", // Replace with your website URL
+  siteUrl: "https://www.communirec.com.com", // Replace with your website URL
   generateRobotsTxt: true, // Generate robots.txt file
   sitemapSize: 7000, // (Optional) Number of URLs per sitemap file
   changefreq: "daily", // (Optional) Frequency of page changes
